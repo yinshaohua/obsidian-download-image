27eb3d784a55ecda1af05d1eecc4910447e50a96
yinshaohua <yinshaohua@gmail.com>
2026-04-09 22:25:35 +0800

    feat(01-01): remove boilerplate and update plugin identity
    - Replace sample-plugin metadata with obsidian-download-image identity
    - Update minAppVersion to 1.5.7 for getAvailablePathForAttachment API
    - Rename MyPluginSettings to DownloadImageSettings
    - Rename SampleSettingTab to DownloadImageSettingTab
    - Update import from MyPlugin to DownloadImagePlugin

--- src/settings.ts
+++ src/settings.ts
@@ -1,18 +1,18 @@
 import {App, PluginSettingTab, Setting} from "obsidian";
-import MyPlugin from "./main";
+import DownloadImagePlugin from "./main";
 
-export interface MyPluginSettings {
+export interface DownloadImageSettings {
 	mySetting: string;
 }
 
-export const DEFAULT_SETTINGS: MyPluginSettings = {
+export const DEFAULT_SETTINGS: DownloadImageSettings = {
 	mySetting: 'default'
 }
 
-export class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export class DownloadImageSettingTab extends PluginSettingTab {
+	plugin: DownloadImagePlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: DownloadImagePlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
